3e8a2795acddf42870ee0b893d9ba9035210ad99
Nicola Saunders <nicola@studio24.net>
2019-11-01 10:07:40 +0000

    Refactor the axiomatic owl and rename as the stack

--- web/index.php
+++ web/index.php
@@ -47,36 +47,27 @@
 <body>
 
 	<?php // Start main ?>
-    <main>
-
-        <div class="content">
-            <h1>The quick brown fox jumps over the lazy dog</h1>
-	        <h2>The quick brown fox jumps over the lazy dog</h2>
-	        <h3>The quick brown fox jumps over the lazy dog</h3>
-	        <h4>The quick brown fox jumps over the lazy dog</h4>
-	        <h5>The quick brown fox jumps over the lazy dog</h5>
-	        <h6>The quick brown fox jumps over the lazy dog</h6>
-            <p>So, let’s see about getting some basic styles down for the reboot of <em>Apollo</em>. I’m going to need some paragraphs with some reasonably-looking text inside of them to try and create a base natural layout, so that I can accurately assess how these <strong>core</strong> styles are working.</p>
-            <p>My thinking here is drawn from the Springer Nature Front End Playbook approach. That is to say:</p>
-            <ul>
-                <li>Serve core CSS styles to all browsers for an accessible experience (perhaps not the most exciting, but it works)</li>
-                <li>Use a CSS media query to cut the mustard and serve advanced CSS styles to modern browsers (plus IE10/11)</li>
-                <li>Load JavaScript according to whether the advanced CSS styles have been applied to the site</li>
-            </ul>
-            <p>I believe this is in the best interests of all web users, and takes into account issues of performance for older browsers by giving them just what they need to make content accessible for users. We infer that users of older browsers may well have a device with less processing power, and so we simplify. We can progressively enhance the experience for users who are priviledged to have access to more capable browsers and devices.</p>
-            <h2>Points to bear in mind</h2>
-            <p>There are certain design schemes that we have tended to follow in recent projects at Studio 24. These are:</p>
-            <ol>
-                <li>Limiting the content width to 90% of the viewport on smaller screens.</li>
-                <li>Expanding this width to 80% of the viewport on mid-sized screens.</li>
-                <li>Setting a max-width for content at 1300px</li>
-                <li>Allowing certain elements to extend beyond these restrictions to fill the browser width (e.g. banner/hero components)</li>
-            </ol>
-            <p>It is worth bearing these points in mind for our base styles, as they should provide a neater reading experience for users. For example, it will provide a nice margin on either side of text on smaller screens, keeping the text away from the very edges.</p>
-            <p>By adjusting Apollo styles locally, I can test the resulting visual feel using <a href="https://www.browserstack.com/users/sign_in">Browserstack</a> and – when happy – push these changes into a separate branch on the Github Apollo repo.</p>
-        </div>
-
-    </main>
+    <main class="stack stack-m">
+        <h1>Overview</h1>
+        <p>So, let’s see about getting some basic styles down for the reboot of <em>Apollo</em>. I’m going to need some paragraphs with some reasonably-looking text inside of them to try and create a base natural layout, so that I can accurately assess how these <strong>core</strong> styles are working.</p>
+        <p>My thinking here is drawn from the Springer Nature Front End Playbook approach. That is to say:</p>
+        <ul>
+            <li>Serve core CSS styles to all browsers for an accessible experience (perhaps not the most exciting, but it works)</li>
+            <li>Use a CSS media query to cut the mustard and serve advanced CSS styles to modern browsers (plus IE10/11)</li>
+            <li>Load JavaScript according to whether the advanced CSS styles have been applied to the site</li>
+        </ul>
+        <p>I believe this is in the best interests of all web users, and takes into account issues of performance for older browsers by giving them just what they need to make content accessible for users. We infer that users of older browsers may well have a device with less processing power, and so we simplify. We can progressively enhance the experience for users who are priviledged to have access to more capable browsers and devices.</p>
+        <h2>Points to bear in mind</h2>
+        <p>There are certain design schemes that we have tended to follow in recent projects at Studio 24. These are:</p>
+        <ol>
+            <li>Limiting the content width to 90% of the viewport on smaller screens.</li>
+            <li>Expanding this width to 80% of the viewport on mid-sized screens.</li>
+            <li>Setting a max-width for content at 1300px</li>
+            <li>Allowing certain elements to extend beyond these restrictions to fill the browser width (e.g. banner/hero components)</li>
+        </ol>
+        <p>It is worth bearing these points in mind for our base styles, as they should provide a neater reading experience for users. For example, it will provide a nice margin on either side of text on smaller screens, keeping the text away from the very edges.</p>
+        <p>By adjusting Apollo styles locally, I can test the resulting visual feel using <a href="https://www.browserstack.com/users/sign_in">Browserstack</a> and – when happy – push these changes into a separate branch on the Github Apollo repo.</p>
+	</main>
 	<?php // End main ?>
 
 <?php require_once( 'delete-this-folder-in-wp/_includes/scripts__footer.html' ); ?>
