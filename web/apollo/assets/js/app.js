4966f4f608540ad34d303007c706c9536414670b
Kiki Lei <chi.mei@studio24.net>
2019-11-05 12:55:50 +0000

    Updated deprecated code

--- web/apollo/assets/js/app.js
+++ web/apollo/assets/js/app.js
@@ -1,25 +1,4 @@
 /**
- * Element.closest polyfill:
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
- */
-if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector ||
-        Element.prototype.webkitMatchesSelector;
-}
-
-if (!Element.prototype.closest) {
-    Element.prototype.closest = function (s) {
-        var el = this;
-        if (!document.documentElement.contains(el)) return null;
-        do {
-            if (el.matches(s)) return el;
-            el = el.parentElement || el.parentNode;
-        } while (el !== null && el.nodeType === 1);
-        return null;
-    };
-}
-
-/**
  * Does it dijon?
  *
  * The matchMedia API is supported by IE10 and IE11.
@@ -77,14 +56,14 @@ if (typeof window.matchMedia !== 'undefined') {
 
     if (matchMedia) {
         var mq = window.matchMedia("(min-width: 1000px)");
-        mq.addListener(myApp.updateARIAroles);
+        mq.addEventListener("change", myApp.updateARIAroles);
     }
 
     var contentToggler = document.querySelectorAll('[data-toggler]');
 
     myApp.updateARIAroles(contentToggler);
 
-    // Collapsibles
+    // Collapsible
     var collapsibleHeading = document.querySelectorAll('.js-collapsible-heading');
 
     myApp.collaps(collapsibleHeading);
