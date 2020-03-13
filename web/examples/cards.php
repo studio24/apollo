7008a5f10efd5b9ca5c7ff3d07ce63e6901825da
Nicola Saunders <nicola@studio24.net>
2020-03-13 17:30:50 +0000

    Refactor card component and examples

--- web/examples/cards.php
+++ web/examples/cards.php
@@ -44,6 +44,11 @@
 		.l-switcher > * > * {
 			min-width: 25%;
 		}
+
+		.sidebar .card {
+			height: auto;
+			width: 100%;
+		}
 	</style>
 
 
@@ -58,11 +63,70 @@
     <?php // Start main ?>
     <main class="l-center">
         <h1>Example: Cards</h1>
-	    <h2>Default layout</h2>
+	    <h2>Basic card</h2>
+	    <ul>
+		    <li>The basic card consists of an image area (using the <a href="../layouts/frame.php">frame layout</a>), and a content area (using the <a href="../layouts/box.php">box layout</a>). If the image is purely decorative, an empty <code>alt</code> attribute should be provided.</li>
+		    <li>The content area appears first in the source order, so that the card is correctly labelled by its heading. We use flexbox to visually put the image ahead of the content area.</li>
+		    <li>Make sure that the heading element fits with the heading order of surrounding content. Headings with an equal or higher rank start a new section, headings with a lower rank start new subsections that are part of the higher ranked section.</li>
+	    </ul>
+	    <div class="l-sidebar">
+		    <div>
+			    <div class="not-sidebar">
+				    <ul>
+					    <li>We want the entire card to act as a link:
+						    <ul>
+							    <li>Avoid wrapping the entire card within a link. This can result in the entire contents of the card being read out to screen reader users.</li>
+							    <li>Avoid wrapping the image in a link and repeating that link elsewhere within the card. This results in unnecessary tab stops and duplicated links.</li>
+							    <li>It makes sense to use the heading text as the link for our card. It is unique and descriptive. Add the link inside the heading.</li>
+							    <li>To make the link span the entire card, we apply <code>position: relative;</code> to the card and <code>position: absolute;</code> to the link's <code>::after</code> pseudo element.</li>
+							    <li>Link affordance is provided by the <code>pointer</code> cursor style. We add a <code>:hover</code> style for further affordance.</li>
+						    </ul>
+					    </li>
+				    </ul>
+			    </div>
+			    <div class="sidebar">
+				    <article class="card">
+						    <div class="l-box l-box--no-border card__text">
+							    <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net">My card title</a></h3>
+							    <p>Here is all the descriptive teaser text for my card.</p>
+						    </div>
+						    <div class="l-frame l-frame--16-9">
+							    <img src="/dist/assets/images/sherlock.jpg" alt>
+						    </div>
+					    </article>
+			    </div>
+		    </div>
+	    </div>
+
+	    <h2>Card with a call to action</h2>
+	    <div class="l-sidebar">
+		    <div>
+			    <div class="not-sidebar">
+				    <ul>
+					    <li>This variation of the basic card includes a visual call to action (<abbr>CTA</abbr>), without adding an unhelpful and generic "Read more" link.</li>
+					    <li>The <abbr>CTA</abbr> is hidden from assistive technologies using the <code>aria-hidden</code> attribute.</li>
+					    <li>For sighted screen reader users, we attach &lsquo;Read more&rsquo; to the link with <code>aria-describedby</code> in conjunction with a unique ID on the <abbr>CTA</abbr>. This description is always announced after the link text.</li>
+					    <li>The content area has a flexbox context with <code>flex-direction: column;</code> courtesy of the stack mixin which creates vertical spacing between sibling elements. This enables us to apply <code>margin-top: auto;</code> to the <abbr>CTA</abbr> to align it to the bottom of the card, which looks more attractive in a list of cards.</li>
+				    </ul>
+			    </div>
+			    <div class="sidebar">
+				    <article class="card card--cta">
+					    <div class="l-box l-box--no-border card__text">
+						    <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="unique-card-title">Unique card title</a></h3>
+						    <p>Here is all the descriptive teaser text for my card.</p>
+						    <span class="button button--primary card__cta" aria-hidden="true" id="unique-card-title-2">Read more</span>
+					    </div>
+					    <div class="l-frame l-frame--16-9">
+						    <img src="/dist/assets/images/sherlock.jpg" alt>
+					    </div>
+				    </article>
+			    </div>
+		    </div>
+	    </div>
         <div class="l-switcher">
-            <ul class="card-list">
+            <ul class="clean-list">
 	            <li>
-		            <article class="card">
+		            <article class="card card--cta">
 		                <div class="l-box l-box--no-border card__text">
 			                <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="unique-card-title-1">Unique card title 1</a></h3>
 		                    <p>Here is all the descriptive teaser text for my card.</p>
@@ -74,7 +138,7 @@
 		            </article>
 	            </li>
 	            <li>
-		            <article class="card">
+		            <article class="card card--cta">
 			            <div class="l-box l-box--no-border card__text">
 				            <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="unique-card-title-2">Unique card title 2</a></h3>
 				            <p>Here is all the descriptive teaser text for my card. But this one has significantly more text content inside it.</p>
@@ -86,7 +150,7 @@
 		            </article>
 	            </li>
 	            <li>
-		            <article class="card">
+		            <article class="card card--cta">
 			            <div class="l-box l-box--no-border card__text">
 				            <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="unique-card-title-3">Unique card title 3 is a bit longer</a></h3>
 				            <p>Here is all the descriptive teaser text for my card.</p>
@@ -98,7 +162,7 @@
 		            </article>
 	            </li>
 	            <li>
-		            <article class="card">
+		            <article class="card card--cta">
 			            <div class="l-box l-box--no-border card__text">
 				            <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="unique-card-title-4">Unique card title 4</a></h3>
 				            <p>Here is all the descriptive teaser text for my card.</p>
@@ -111,10 +175,18 @@
 	            </li>
             </ul>
         </div>
-	    <h2>With additional links, e.g. tags</h2>
-	    <div class="l-switcher">
-		    <ul class="clean-list card-list">
-			    <li>
+
+	    <h2>Card with extra links</h2>
+	    <div class="l-sidebar">
+		    <div>
+			    <div class="not-sidebar">
+				    <ul>
+					    <li>Sometimes we need to include additional links e.g. for category indexes or author attributions.</li>
+					    <li>By giving such links <code>position: relative;</code> and <code>z-index: 10;</code> we can raise them above the level of the card link and provide access to users.</li>
+					    <li>In this example, tags are included using a modification of the <a href="../layouts/cluster.php">cluster layout</a></li>
+				    </ul>
+			    </div>
+			    <div class="sidebar">
 				    <article class="card">
 					    <div class="l-box l-box--no-border card__text">
 						    <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="another-card-title-1">Another card title 1</a></h3>
@@ -139,45 +211,10 @@
 						    <img src="/dist/assets/images/sherlock.jpg" alt>
 					    </div>
 				    </article>
-			    </li>
-			    <li>
-				    <article class="card">
-					    <div class="l-box l-box--no-border card__text">
-						    <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="another-card-title-2">Another card title 2</a></h3>
-						    <p>Here is all the descriptive teaser text for my card.</p>
-						    <span class="button button--primary card__cta" aria-hidden="true" id="another-card-title-2">Read more</span>
-					    </div>
-					    <div class="l-frame l-frame--16-9">
-						    <img src="/dist/assets/images/sherlock.jpg" alt>
-					    </div>
-				    </article>
-			    </li>
-			    <li>
-				    <article class="card">
-					    <div class="l-box l-box--no-border card__text">
-						    <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="another-card-title-3">Another card title 3</a></h3>
-						    <p>Here is all the descriptive teaser text for my card.</p>
-						    <span class="button button--primary card__cta" aria-hidden="true" id="another-card-title-3">Read more</span>
-					    </div>
-					    <div class="l-frame l-frame--16-9">
-						    <img src="/dist/assets/images/sherlock.jpg" alt>
-					    </div>
-				    </article>
-			    </li>
-			    <li>
-				    <article class="card">
-					    <div class="l-box l-box--no-border card__text">
-						    <h3 class="card__heading"><a class="card__link" href="https://www.studio24.net" aria-describedby="another-card-title-4">Another card title 4</a></h3>
-						    <p>Here is all the descriptive teaser text for my card.</p>
-						    <span class="button button--primary card__cta" aria-hidden="true" id="another-card-title-4">Read more</span>
-					    </div>
-					    <div class="l-frame l-frame--16-9">
-						    <img src="/dist/assets/images/sherlock.jpg" alt>
-					    </div>
-				    </article>
-			    </li>
-		    </ul>
+			    </div>
+		    </div>
 	    </div>
+
     </main>
     <?php // End main ?>
 </div>
