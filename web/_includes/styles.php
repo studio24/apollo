feb345c3af7ed82b584596b53069b7abf7117479
Simon R Jones <simon@studio24.net>
2020-05-22 14:19:40 +0100

    Add reference for CSS cutting the mustard

--- web/_includes/styles.php
+++ web/_includes/styles.php
@@ -1,6 +1,10 @@
     <link rel="stylesheet" href="/dist/assets/styles/core.min.css" type="text/css" media="screen"/>
     <?php
-    // Array of media queries to use with CSS cutting the mustard
+    /**
+     * Array of media queries to use with CSS cutting the mustard
+     * Combine on one line for Edge compatibility
+     * @see https://www.sitepoint.com/cutting-the-mustard-with-css-media-queries/
+     */
     $mediaQueries = [
         'only screen and (-webkit-min-device-pixel-ratio:0) and (min-color-index:0)',
         '(-ms-high-contrast: none)',
