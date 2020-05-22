5334211896591c9646a1bcc2cc0ee18791b03944
Nicola Saunders <nicola@studio24.net>
2020-05-22 17:51:04 +0100

    Add image credit

--- web/layouts/frame.php
+++ web/layouts/frame.php
@@ -49,12 +49,22 @@
 <div class="wrap">
     <?php // Start main ?>
     <main>
-        <h1>Layout: Frame</h1>
-        <p>The frame layout is useful for cropping media (images and video) to a specific aspect ratio. It uses percentage padding to create a frame with a set aspect ratio (because padding is always based on the width of an element), which then acts as a window onto the content inside.</p>
 
-        <div class="l-sidebar ex1">
+        <h1>Layout: Frame</h1>
+
+        <div class="l-sidebar">
             <div>
                 <div class="not-sidebar">
+                    <p>The frame layout is useful for cropping media (images and video) to a specific aspect ratio. It uses percentage padding to create a frame with a set aspect ratio (because padding is always based on the width of an element), which then acts as a window onto the content inside.</p>
+                    <p>It is not recommended to apply the <code>.l-frame</code> class to the <code>figure</code> element, as this will obscure the <code>figcaption</code> if used. Instead, place it on an inner <code>picture</code> or <code>div</code> element.</p>
+                    <p>Because <code>.l-frame</code> has a Flexbox context, we use <code>align-items: center;</code> and <code>justify-content: center;</code> to centre the child item both vertically and horizontally.</p>
+                    <h2>Internet Explorer/Edge considerations</h2>
+                    <p>The default frame layout uses <code>object-fit: cover</code> for images and videos. This is not supported in Internet Explorer or Edge prior to version 16 and in Edge 16-18 it is only supported on images. However, the Chromium Edge browser, which does support object-fit, is now available to systems as far back as Windows 7.</p>
+                    <p>For images, a simple JavaScript workaround is available that swaps the image for a background-image instead, although this removes the accessibility benefits offered by images. Alternatively, you may prefer to forgo <code>object-fit</code> and adapt your instance of the frame to rely on absolute positioning and hiding any overflow.</p>
+                    <h2>Image credit</h2>
+                    <p>Photo by <a href="https://unsplash.com/photos/-zASKXkwkIY">Steve Johnson</a> on <a href="https://unsplash.com/">Unsplash</a></p>
+                </div>
+                <div class="sidebar">
                     <figure>
                         <div class="l-frame l-frame--16-9">
                             <img src="/dist/assets/images/steve-johnson--zASKXkwkIY-unsplash.jpg" alt="Multicoloured abstract painting" />
@@ -82,13 +92,7 @@
                         </div>
                         <figcaption>Square frame uses the class <code>.l-frame--square</code></figcaption>
                     </figure>
-                </div>
-                <div class="sidebar">
-                    <p>It is not recommended to apply the <code>.l-frame</code> class to the <code>figure</code> element, as this will obscure the <code>figcaption</code> if used. Instead, place it on an inner <code>picture</code> or <code>div</code> element.</p>
-                    <p>Because <code>.l-frame</code> has a Flexbox context, we use <code>align-items: center;</code> and <code>justify-content: center;</code> to centre the child item both vertically and horizontally.</p>
-                    <h2>Internet Explorer/Edge considerations</h2>
-                    <p>The default frame layout uses <code>object-fit: cover</code> for images and videos. This is not supported in Internet Explorer or Edge prior to version 16 and in Edge 16-18 it is only supported on images. However, the Chromium Edge browser, which does support object-fit, is now available to systems as far back as Windows 7.</p>
-                    <p>For images, a simple JavaScript workaround is available that swaps the image for a background-image instead, although this removes the accessibility benefits offered by images. Alternatively, you may prefer to forgo <code>object-fit</code> and adapt your instance of the frame to rely on absolute positioning and hiding any overflow.</p>
+
                 </div>
             </div>
         </div>
